3e6c84b23d8ef43d538fe00636b2ffee336f5796
mark-5 <maf@cpan.org>
2020-10-16 16:24:11 -0500

    match

--- build/check_zk_version.h
+++ build/check_zk_version.h
@@ -18,9 +18,11 @@
  */
 
 /* keep in sync with Makefile.PL */
-#if !defined(ZOO_VERSION)
-# if !defined(ZOO_MAJOR_VERSION) || ZOO_MAJOR_VERSION != 3 || \
+#if defined(ZOO_VERSION)
+/* ZOO_VERSION was introduced by ZOOKEEPER-3635 (3.6.0-pre). */
+#else /* !defined(ZOO_VERSION) */
+#if !defined(ZOO_MAJOR_VERSION) || ZOO_MAJOR_VERSION != 3 || \
     !defined(ZOO_MINOR_VERSION) || ZOO_MINOR_VERSION < 2
-#  error "Net::ZooKeeper requires at least ZooKeeper version 3.2.0"
-# endif
+#error "Net::ZooKeeper requires at least ZooKeeper version 3.2.0"
 #endif
+#endif /* !defined(ZOO_VERSION) */
